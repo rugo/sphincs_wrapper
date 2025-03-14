1867d52a73a3cde56cf78f63b104d6457274712e
Ruben Gonzalez <ruben@neodyme.io>
2025-03-14 10:50:52 +0100

    Remove useless comments

--- build.rs
+++ build.rs
@@ -5,9 +5,6 @@ use std::path::PathBuf;
 
 
 fn main() {
-    // address.c randombytes.c merkle.c wots.c wotsx1.c utils.c utilsx1.c fors.c sign.c
-    // fips202.c hash_shake.c thash_shake_robust.c
-    // params.h address.h randombytes.h merkle.h wots.h wotsx1.h utils.h utilsx1.h fors.h api.h  hash.h thash.h
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     // Generate Rust bindings using bindgen
@@ -23,20 +20,6 @@ fn main() {
         .expect("Couldn't write bindings!");
     
     Build::new()
-    // .file("src_c/ref/hash.h")
-    // .file("src_c/ref/params.h")
-    // .file("src_c/ref/address.h")
-    
-    // // .file("src_c/ref/merkle.h")
-    // .file("src_c/ref/wots.h")
-    // // .file("src_c/ref/wotsx1.h")
-    // .file("src_c/ref/utils.h")
-    // .file("src_c/ref/utilsx1.h")
-    // .file("src_c/ref/fors.h")
-    // .file("src_c/ref/api.h")
-    // .file("src_c/ref/hash.h")
-    // .file("src_c/ref/thash.h")
-        // .file("src_c/ref/randombytes.h")
         .file("src_c/ref/params/params-sphincs-sha2-tests.h")
         .file("src_c/ref/randombytes.c")
         .file("src_c/ref/address.c")
@@ -50,9 +33,6 @@ fn main() {
         .file("src_c/ref/sha2.c")
         .file("src_c/ref/hash_sha2.c")
         .file("src_c/ref/thash_sha2_simple.c")
-        //.file("src_c/ref/fips202.c")
-        //.file("src_c/ref/hash_shake.c")
-        //.file("src_c/ref/thash_shake_robust.c")
         .include("src_c/ref")
         .flag("-std=c99")
         .flag("-DPARAMS=sphincs-sha2-tests")
